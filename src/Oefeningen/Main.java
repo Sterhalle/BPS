edf0021ae2afdc879248e874d35107c136117855
Sterhalle <sterhalle@outlook.com>
2021-06-02 18:26:39 +0200

    Added a Main file for practice.

---
+++ src/Oefeningen/Main.java
@@ -0,0 +1,28 @@
+package Oefeningen;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age;
+        String voornaam;
+        String achternaam;
+
+        Scanner leeftijd = new Scanner(System.in);
+        Scanner naam = new Scanner(System.in);
+
+        System.out.println("Geef uw leeftijd op");
+        age = leeftijd.nextInt();
+
+
+
+
+
+
+
+
+
+
+    }
+}
