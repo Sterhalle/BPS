01dabd3080080f1152c4638a71e6f0cae0ce2872
Sterhalle <sterhalle@outlook.com>
2021-06-04 16:59:39 +0200

    Update Main.java

--- src/Oefeningen/Main.java
+++ src/Oefeningen/Main.java
@@ -5,15 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int age;
-        String voornaam;
-        String achternaam;
 
-        Scanner leeftijd = new Scanner(System.in);
-        Scanner naam = new Scanner(System.in);
-
-        System.out.println("Geef uw leeftijd op");
-        age = leeftijd.nextInt();
 
 
 
